620800bced620f397ab68b37ab50b853284332d3
Cameron Mullins <cmullins510@gmail.com>
2020-04-01 15:51:23 -0700

    initial commit

---
+++ arithmetic/arithmetic.js
@@ -0,0 +1,21 @@
+// Write the logic for each function that will return the given arithmetic described by the function name
+const add = function (num1, num2) {
+
+};
+
+const subtract = function (num1, num2) {
+
+};
+
+const multiply = function (num1, num2) {
+
+};
+
+const divide = function (num1, num2) {
+
+};
+
+// Invoke each function with sample numbers to test
+
+
+// As a bonus, add a form to the html file that will allow a user to input the two number parameters
